2859dbb2f18a0ca0ffad628b46c8b28e7b06c5cb
soobin im <40817586+Soobinnn@users.noreply.github.com>
2020-01-08 11:40:01 +0900

    Update jenkins_pipeline_sample.groovy

--- jenkins_pipeline_sample.groovy
+++ jenkins_pipeline_sample.groovy
@@ -1,1 +1,21 @@
+pipeline {
+  agent {
+    label 'jenkins-pipeline-sample'
+  }
+  
+  environment {
+  //     REGISTRY_HOST=""
+  //     REGISTRY_PORT=""
+  //     REPOSITORY_NAME="${env.SERVER_TYPE == "prod" ? "${PROJECT_NAME}" : "${PROJECT_NAME}-${SERVER_TYPE}"}"
+  }
 
+  stages {
+     stage("Set Current Build DisplayName") {
+      steps {
+        script {
+          currentBuild.displayName = "${SERVER_TYPE}_${BRANCH_NAME}_${EXECUTOR}"
+        }
+      }
+     }
+  }
+}
